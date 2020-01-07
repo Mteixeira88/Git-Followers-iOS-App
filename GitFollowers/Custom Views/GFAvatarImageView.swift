d1b1328570cfaf40d8484c31de0960a6595be55d
Miguel Teixeira <miguel.teixeira@namecheap.com>
2020-01-07 16:58:00 +0000

    14 - UICollectionView - Custom Follower Cell

---
+++ GitFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,31 @@
+//
+//  GFAvatarImageView.swift
+//  GitFollowers
+//
+//  Created by Miguel Teixeira on 07/01/2020.
+//  Copyright © 2020 Miguel Teixeira. All rights reserved.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}

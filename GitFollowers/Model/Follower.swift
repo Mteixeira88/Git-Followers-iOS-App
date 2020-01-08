74abd6c29f0a56a79403abd02abf4721d1ff25d9
Miguel Teixeira <miguel.teixeira@namecheap.com>
2020-01-08 11:03:24 +0000

    16 - UICollectionView - Diffable Data Source

--- GitFollowers/Model/Follower.swift
+++ GitFollowers/Model/Follower.swift
@@ -9,11 +9,16 @@
 import Foundation
 
 // Because it performs to Codable, the variable names have to be exatly the same as the server response
-struct Follower: Codable {
+struct Follower: Codable, Hashable {
     var login: String
     //On the response it comes "avatar_url" but the Swift Code Best Practices
     // says to use camelCase instead of snake_case,
     // JSON decoder will convert automatically the snake_case's to camelCase's
     // but the variable name must have the exact same name
     var avatarUrl: String
+    
+    // Convert to unique id
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(login)
+    }
 }

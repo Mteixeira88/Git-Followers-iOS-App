cd782c4eb9bf26375085a7bf5eccc10f90ac6413
Miguel Teixeira <miguel.teixeira@namecheap.com>
2020-01-08 11:44:21 +0000

    18 - Downloading & Caching Avatar Images

--- GitFollowers/Custom Views/Cells/FollowerCell.swift
+++ GitFollowers/Custom Views/Cells/FollowerCell.swift
@@ -25,6 +25,7 @@ class FollowerCell: UICollectionViewCell {
     
     func set(follower: Follower) {
         usernameLabel.text = follower.login
+        avatarImageView.downloadImage(from: follower.avatarUrl)
     }
     
     private func configure() {

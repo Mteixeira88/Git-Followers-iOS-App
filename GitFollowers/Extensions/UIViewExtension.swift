d2554b87dcf42584144649a7058267f51fea2646
Miguel Teixeira <mteixeira88013@gmail.com>
2020-02-06 15:37:03 +0000

    iPhone SE - UserInfoVC Fixes

--- GitFollowers/Extensions/UIViewExtension.swift
+++ GitFollowers/Extensions/UIViewExtension.swift
@@ -14,4 +14,14 @@ extension UIView {
             addSubview(view)
         }
     }
+    
+    func pinToEdges(of superview: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superview.topAnchor),
+            leadingAnchor.constraint(equalTo: superview.leadingAnchor),
+            trailingAnchor.constraint(equalTo: superview.trailingAnchor),
+            bottomAnchor.constraint(equalTo: superview.bottomAnchor),
+        ])
+    }
 }
